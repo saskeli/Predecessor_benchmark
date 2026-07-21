2d9c177e4f4ba4af8fea58cb79f21a83dc995a68
Saska Dönges <saskeli@kapsi.fi>
2026-07-21 15:03:28 +0300

    don't try to do 1M modifications on std::flat_set

--- bench.cpp
+++ bench.cpp
@@ -4,6 +4,7 @@
 #include <flat_set>
 #include <random>
 #include <set>
+#include <type_traits>
 #include <unordered_set>
 #include <vector>
 
@@ -12,7 +13,9 @@
 #include "fast_B-trees/include/static_search.hpp"
 #include "op_templates.hpp"
 
-#define NUMS ->Arg(1000)->Arg(10000)->Arg(100000)->Arg(1000000)->Arg(10000000)->Arg(100000000)
+#define NUMS                                                              \
+  ->Arg(1000)->Arg(10000)->Arg(100000)->Arg(1000000)->Arg(10000000)->Arg( \
+      100000000)
 
 template <class set_t, class T>
 void BM_pred(benchmark::State& state) {
@@ -31,7 +34,7 @@ void BM_pred(benchmark::State& state) {
   state.SetLabel(std::to_string(checksum));
   state.SetItemsProcessed(state.iterations() * queries);
 }
-template<class set_t, class T>
+template <class set_t, class T>
 void BM_contains(benchmark::State& state) {
   size_t n = state.range(0);
   const constexpr size_t queries = q_count;
@@ -47,7 +50,7 @@ void BM_contains(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * queries);
 }
 
-template<class set_t, class T>
+template <class set_t, class T>
 void BM_access(benchmark::State& state) {
   size_t n = state.range(0);
   const constexpr size_t queries = q_count;
@@ -64,7 +67,7 @@ void BM_access(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * queries);
 }
 
-template<class set_t, class T>
+template <class set_t, class T>
 void BM_sum(benchmark::State& state) {
   size_t n = state.range(0);
   std::vector<T> q_vec;
@@ -80,10 +83,12 @@ void BM_sum(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * set.size());
 }
 
-template<class set_t, class T>
+template <class set_t, class T>
 void BM_insert(benchmark::State& state) {
   size_t n = state.range(0);
-  const size_t queries = std::min(q_count, n);
+  const size_t queries = std::is_same<set_t, std::flat_set<T>>::value
+                             ? 1000
+                             : std::min(q_count, n);
   std::vector<T> q_vec;
   set_t set = pt::with<set_t, T>::build(n - queries / 2, 0, queries, q_vec);
 
@@ -102,11 +107,12 @@ void BM_insert(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * queries);
 }
 
-
-template<class set_t, class T>
+template <class set_t, class T>
 void BM_remove(benchmark::State& state) {
   size_t n = state.range(0);
-  const size_t queries = std::min(q_count, n);
+  const size_t queries = std::is_same<set_t, std::flat_set<T>>::value
+                             ? 1000
+                             : std::min(q_count, n);
   std::vector<T> q_vec;
   set_t set = pt::with<set_t, T>::build(n + queries / 2, queries, 0, q_vec);
 
@@ -125,122 +131,155 @@ void BM_remove(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * queries);
 }
 #ifdef TEST_STD_SET_INT
-BENCHMARK(BM_pred<std::set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_sum<std::set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_access<std::set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_contains<std::set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_remove<std::set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_insert<std::set<int64_t>, int64_t>)NUMS;
+BENCHMARK(BM_pred<std::set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_sum<std::set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_access<std::set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_contains<std::set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_remove<std::set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_insert<std::set<int64_t>, int64_t>) NUMS;
 #endif
 #ifdef TEST_STD_SET_DOUBLE
-BENCHMARK(BM_pred<std::set<double>, double>)NUMS;
-BENCHMARK(BM_sum<std::set<double>, double>)NUMS;
-BENCHMARK(BM_access<std::set<double>, double>)NUMS;
-BENCHMARK(BM_contains<std::set<double>, double>)NUMS;
-BENCHMARK(BM_remove<std::set<double>, double>)NUMS;
-BENCHMARK(BM_insert<std::set<double>, double>)NUMS;
+BENCHMARK(BM_pred<std::set<double>, double>) NUMS;
+BENCHMARK(BM_sum<std::set<double>, double>) NUMS;
+BENCHMARK(BM_access<std::set<double>, double>) NUMS;
+BENCHMARK(BM_contains<std::set<double>, double>) NUMS;
+BENCHMARK(BM_remove<std::set<double>, double>) NUMS;
+BENCHMARK(BM_insert<std::set<double>, double>) NUMS;
 #endif
 #ifdef TEST_STD_SET_DEC
-BENCHMARK(BM_pred<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_sum<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_access<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_contains<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_remove<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_insert<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
+BENCHMARK(BM_pred<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>) NUMS;
+BENCHMARK(BM_sum<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>) NUMS;
+BENCHMARK(BM_access<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>) NUMS;
+BENCHMARK(BM_contains<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_remove<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>) NUMS;
+BENCHMARK(BM_insert<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>) NUMS;
 #endif
 
 #ifdef TEST_STD_FLAT_SET_INT
-BENCHMARK(BM_pred<std::flat_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_sum<std::flat_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_access<std::flat_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_contains<std::flat_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_remove<std::flat_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_insert<std::flat_set<int64_t>, int64_t>)NUMS;
+BENCHMARK(BM_pred<std::flat_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_sum<std::flat_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_access<std::flat_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_contains<std::flat_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_remove<std::flat_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_insert<std::flat_set<int64_t>, int64_t>) NUMS;
 #endif
 #ifdef TEST_STD_FLAT_SET_DOUBLE
-BENCHMARK(BM_pred<std::flat_set<double>, double>)NUMS;
-BENCHMARK(BM_sum<std::flat_set<double>, double>)NUMS;
-BENCHMARK(BM_access<std::flat_set<double>, double>)NUMS;
-BENCHMARK(BM_contains<std::flat_set<double>, double>)NUMS;
-BENCHMARK(BM_remove<std::flat_set<double>, double>)NUMS;
-BENCHMARK(BM_insert<std::flat_set<double>, double>)NUMS;
+BENCHMARK(BM_pred<std::flat_set<double>, double>) NUMS;
+BENCHMARK(BM_sum<std::flat_set<double>, double>) NUMS;
+BENCHMARK(BM_access<std::flat_set<double>, double>) NUMS;
+BENCHMARK(BM_contains<std::flat_set<double>, double>) NUMS;
+BENCHMARK(BM_remove<std::flat_set<double>, double>) NUMS;
+BENCHMARK(BM_insert<std::flat_set<double>, double>) NUMS;
 #endif
 #ifdef TEST_STD_FLAT_SET_DEC
-BENCHMARK(BM_pred<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_sum<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_access<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_contains<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_remove<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_insert<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
+BENCHMARK(BM_pred<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_sum<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_access<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_contains<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_remove<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_insert<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
 #endif
 
 #ifdef TEST_ABSL_BTREE_SET_INT
-BENCHMARK(BM_pred<absl::btree_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_sum<absl::btree_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_access<absl::btree_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_contains<absl::btree_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_remove<absl::btree_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_insert<absl::btree_set<int64_t>, int64_t>)NUMS;
+BENCHMARK(BM_pred<absl::btree_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_sum<absl::btree_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_access<absl::btree_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_contains<absl::btree_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_remove<absl::btree_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_insert<absl::btree_set<int64_t>, int64_t>) NUMS;
 #endif
 #ifdef TEST_ABSL_BTREE_SET_DOUBLE
-BENCHMARK(BM_pred<absl::btree_set<double>, double>)NUMS;
-BENCHMARK(BM_sum<absl::btree_set<double>, double>)NUMS;
-BENCHMARK(BM_access<absl::btree_set<double>, double>)NUMS;
-BENCHMARK(BM_contains<absl::btree_set<double>, double>)NUMS;
-BENCHMARK(BM_remove<absl::btree_set<double>, double>)NUMS;
-BENCHMARK(BM_insert<absl::btree_set<double>, double>)NUMS;
+BENCHMARK(BM_pred<absl::btree_set<double>, double>) NUMS;
+BENCHMARK(BM_sum<absl::btree_set<double>, double>) NUMS;
+BENCHMARK(BM_access<absl::btree_set<double>, double>) NUMS;
+BENCHMARK(BM_contains<absl::btree_set<double>, double>) NUMS;
+BENCHMARK(BM_remove<absl::btree_set<double>, double>) NUMS;
+BENCHMARK(BM_insert<absl::btree_set<double>, double>) NUMS;
 #endif
 #ifdef TEST_ABSL_BTREE_SET_DEC
-BENCHMARK(BM_pred<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_sum<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_access<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_contains<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_remove<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_insert<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
+BENCHMARK(BM_pred<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_sum<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_access<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_contains<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_remove<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_insert<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
 #endif
 
 #ifdef TEST_BT_DYNAMIC_SET_INT
-BENCHMARK(BM_pred<bt::dynamic_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_sum<bt::dynamic_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_access<bt::dynamic_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_contains<bt::dynamic_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_remove<bt::dynamic_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_insert<bt::dynamic_set<int64_t>, int64_t>)NUMS;
+BENCHMARK(BM_pred<bt::dynamic_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_sum<bt::dynamic_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_access<bt::dynamic_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_contains<bt::dynamic_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_remove<bt::dynamic_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_insert<bt::dynamic_set<int64_t>, int64_t>) NUMS;
 #endif
 #ifdef TEST_BT_DYNAMIC_SET_DOUBLE
-BENCHMARK(BM_pred<bt::dynamic_set<double>, double>)NUMS;
-BENCHMARK(BM_sum<bt::dynamic_set<double>, double>)NUMS;
-BENCHMARK(BM_access<bt::dynamic_set<double>, double>)NUMS;
-BENCHMARK(BM_contains<bt::dynamic_set<double>, double>)NUMS;
-BENCHMARK(BM_remove<bt::dynamic_set<double>, double>)NUMS;
-BENCHMARK(BM_insert<bt::dynamic_set<double>, double>)NUMS;
+BENCHMARK(BM_pred<bt::dynamic_set<double>, double>) NUMS;
+BENCHMARK(BM_sum<bt::dynamic_set<double>, double>) NUMS;
+BENCHMARK(BM_access<bt::dynamic_set<double>, double>) NUMS;
+BENCHMARK(BM_contains<bt::dynamic_set<double>, double>) NUMS;
+BENCHMARK(BM_remove<bt::dynamic_set<double>, double>) NUMS;
+BENCHMARK(BM_insert<bt::dynamic_set<double>, double>) NUMS;
 #endif
 #ifdef TEST_BT_DYNAMIC_SET_DEC
-BENCHMARK(BM_pred<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_sum<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_access<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_contains<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_remove<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_insert<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
+BENCHMARK(BM_pred<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_sum<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_access<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_contains<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_remove<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_insert<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
 #endif
 
 #ifdef TEST_BT_STATIC_SET_INT
-BENCHMARK(BM_pred<bt::static_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_sum<bt::static_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_access<bt::static_set<int64_t>, int64_t>)NUMS;
-BENCHMARK(BM_contains<bt::static_set<int64_t>, int64_t>)NUMS;
+BENCHMARK(BM_pred<bt::static_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_sum<bt::static_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_access<bt::static_set<int64_t>, int64_t>) NUMS;
+BENCHMARK(BM_contains<bt::static_set<int64_t>, int64_t>) NUMS;
 #endif
 #ifdef TEST_BT_STATIC_SET_DOUBLE
-BENCHMARK(BM_pred<bt::static_set<double>, double>)NUMS;
-BENCHMARK(BM_sum<bt::static_set<double>, double>)NUMS;
-BENCHMARK(BM_access<bt::static_set<double>, double>)NUMS;
-BENCHMARK(BM_contains<bt::static_set<double>, double>)NUMS;
+BENCHMARK(BM_pred<bt::static_set<double>, double>) NUMS;
+BENCHMARK(BM_sum<bt::static_set<double>, double>) NUMS;
+BENCHMARK(BM_access<bt::static_set<double>, double>) NUMS;
+BENCHMARK(BM_contains<bt::static_set<double>, double>) NUMS;
 #endif
 #ifdef TEST_BT_STATIC_SET_DEC
-BENCHMARK(BM_pred<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_sum<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_access<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
-BENCHMARK(BM_contains<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)NUMS;
+BENCHMARK(BM_pred<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_sum<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(BM_access<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
+BENCHMARK(
+    BM_contains<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>)
+NUMS;
 #endif
 
 BENCHMARK_MAIN();
--- profile.cpp
+++ profile.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <random>
 #include <set>
+#include <type_traits>
 #include <unordered_set>
 #include <vector>
 
@@ -12,7 +13,6 @@
 #include "counters/counters.hpp"
 #include "fast_B-trees/include/dynamic_search.hpp"
 #include "fast_B-trees/include/static_search.hpp"
-
 #include "op_templates.hpp"
 
 namespace pt {
@@ -43,7 +43,7 @@ void contains(counter_t& counter, uint64_t n) {
   with<set_t, T>::print_ds();
   set_t set = with<set_t, T>::build(n, queries / 2, queries / 2, q_vec);
   print_dt<T>();
-  
+
   int64_t checksum = 0;
   counter.clear();
   for (auto v : q_vec) {
@@ -95,7 +95,9 @@ void sum(counter_t& counter, uint64_t n) {
 
 template <class set_t, class T, class counter_t>
 void insert(counter_t& counter, uint64_t n) {
-  const size_t queries = std::min(q_count, n);
+  const size_t queries = std::is_same<set_t, std::flat_set<T>>::value
+                             ? 1000
+                             : std::min(q_count, n);
   std::vector<T> q_vec;
   with<set_t, T>::print_ds();
   set_t set = with<set_t, T>::build(n - queries / 2, 0, queries, q_vec);
@@ -103,7 +105,8 @@ void insert(counter_t& counter, uint64_t n) {
   counter.clear();
   int64_t checksum = 0;
   for (auto v : q_vec) {
-    checksum += with<set_t, T>::insert(set, v);;
+    checksum += with<set_t, T>::insert(set, v);
+    ;
   }
   counter.accumulate();
   std::cout << "Size: " << n << "\n"
@@ -112,9 +115,11 @@ void insert(counter_t& counter, uint64_t n) {
   std::cout << "Checksum: " << checksum << std::endl;
 }
 
-template <class set_t, class T ,class counter_t>
+template <class set_t, class T, class counter_t>
 void remove(counter_t& counter, uint64_t n) {
-  const size_t queries = std::min(q_count, n);
+  const size_t queries = std::is_same<set_t, std::flat_set<T>>::value
+                             ? 1000
+                             : std::min(q_count, n);
   std::vector<T> q_vec;
   with<set_t, T>::print_ds();
   set_t set = with<set_t, T>::build(n + queries / 2, queries, 0, q_vec);
@@ -138,7 +143,8 @@ int main() {
                   count::Counter::branches, count::Counter::branch_miss,
                   count::Counter::L1D_miss, count::Counter::IPC>
       counter;
-  std::array<uint64_t, 6> sizes = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+  std::array<uint64_t, 6> sizes = {1000,    10000,    100000,
+                                   1000000, 10000000, 100000000};
   for (auto n : sizes) {
 #ifdef TEST_STD_SET_INT
     pt::pred<std::set<int64_t>, int64_t>(counter, n);
@@ -158,11 +164,15 @@ int main() {
 #endif
 #ifdef TEST_STD_SET_DEC
     pt::pred<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::contains<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::access<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::contains<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                       n);
+    pt::access<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                     n);
     pt::sum<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::insert<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::remove<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::insert<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                     n);
+    pt::remove<std::set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                     n);
 #endif
 
 #ifdef TEST_BT_DYNAMIC_SET_INT
@@ -182,12 +192,18 @@ int main() {
     pt::remove<bt::dynamic_set<double>, double>(counter, n);
 #endif
 #ifdef TEST_BT_DYNAMIC_SET_DEC
-    pt::pred<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::contains<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::access<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::sum<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::insert<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::remove<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::pred<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::contains<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::access<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::sum<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::insert<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::remove<bt::dynamic_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
 #endif
 
 #ifdef TEST_ABSL_BTREE_SET_INT
@@ -207,12 +223,18 @@ int main() {
     pt::remove<absl::btree_set<double>, double>(counter, n);
 #endif
 #ifdef TEST_ABSL_BTREE_SET_DEC
-    pt::pred<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::contains<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::access<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::sum<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::insert<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::remove<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::pred<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::contains<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::access<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::sum<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::insert<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::remove<absl::btree_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
 #endif
 
 #ifdef TEST_STD_FLAT_SET_INT
@@ -232,12 +254,18 @@ int main() {
     pt::remove<std::flat_set<double>, double>(counter, n);
 #endif
 #ifdef TEST_STD_FLAT_SET_DEC
-    pt::pred<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::contains<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::access<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::sum<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::insert<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::remove<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::pred<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                        n);
+    pt::contains<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::access<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::sum<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                       n);
+    pt::insert<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::remove<std::flat_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
 #endif
 
 #ifdef TEST_BT_STATIC_SET_INT
@@ -253,10 +281,14 @@ int main() {
     pt::sum<bt::static_set<double>, double>(counter, n);
 #endif
 #ifdef TEST_BT_STATIC_SET_DEC
-    pt::pred<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::contains<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::access<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
-    pt::sum<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter, n);
+    pt::pred<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::contains<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::access<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(
+        counter, n);
+    pt::sum<bt::static_set<pt::Decimal<int64_t>>, pt::Decimal<int64_t>>(counter,
+                                                                        n);
 #endif
   }
   return 0;
